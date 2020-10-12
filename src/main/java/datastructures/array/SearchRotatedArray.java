d9f04fb7b0a5734947474478878ece1431d715b2
Kushal Roy <kuroy@paypal.com>
2020-10-13 02:40:02 +0530

    Search rotated array

--- src/main/java/datastructures/array/SearchRotatedArray.java
+++ src/main/java/datastructures/array/SearchRotatedArray.java
@@ -46,9 +46,39 @@ public class SearchRotatedArray {
     }
   }
 
+  public int search(int[] nums, int target) {
+    int l = 0, r = nums.length - 1;
+
+    while (l < r) {
+      int mid = l + (r - l) / 2;
+
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > nums[l]) {
+        //If left side is sorted
+        if (nums[mid] > target && target >= nums[l]) {
+          r = mid - 1;
+        } else {
+          l = mid + 1;
+        }
+      } else {
+        //Right side is sorted
+        if (nums[mid] < target && nums[r] >= target) {
+          l = mid + 1;
+        } else {
+          r = mid - 1;
+        }
+      }
+    }
+
+    return -1;
+  }
+
   public static void main(String[] args) {
     int[] arr = {4, 5, 6, 7, 8, 9, 1, 2, 3};
     SearchRotatedArray sra = new SearchRotatedArray();
     System.out.println(sra.search(arr, 0, arr.length, 6));
+    System.out.println(sra.search(arr, 6));
+
   }
 }
